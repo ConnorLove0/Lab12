9efd3269658d4c2f40359a836ebf91819edd1efd
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2023-04-11 16:28:52 -0400

    Update AbsCustomer.java

--- src/cpsc2150/banking/models/AbsCustomer.java
+++ src/cpsc2150/banking/models/AbsCustomer.java
@@ -1,2 +1,35 @@
-package cpsc2150.banking.models;public class AbsCustomer {
+package cpsc2150.banking.models;
+
+/**
+ * This class uses toString() override to provide a string representation for customer objects.
+ * @author Connor Love
+ * @since 4/5/2023
+ */
+public abstract class AbsCustomer implements ICustomer {
+
+    // Every customer will have an IMortgage Loan
+    // Protected so the child classes can access it
+    protected IMortgage loan;
+
+    /**
+     * This method overrides toString() to provide a string
+     * representation for customer objects.
+     *
+     * @return a string representation of the customer including loan details
+     *
+     * @post toString = [ A string representation of the customer ]
+     */
+    @Override
+    public String toString() {
+        String str = "";
+        str += "Name: " + getName() + "\n";
+        str += "Income: $" + getIncome() + "\n";
+        str += "Credit Score: " + getCreditScore() + "\n";
+        str += "Monthly Debt: $" + getMonthlyDebtPayments() + "\n";
+        str += "Mortgage info:";
+        if (appliedForLoan()) {
+            str += loan.toString();
+        }
+        return str;
+    }
 }
