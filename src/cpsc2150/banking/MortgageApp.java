ed1a401eee042fed123616f9053cae7bc2276580
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2023-04-11 16:32:16 -0400

    Update MortgageApp.java

--- src/cpsc2150/banking/MortgageApp.java
+++ src/cpsc2150/banking/MortgageApp.java
@@ -1,2 +1,18 @@
-package cpsc2150.banking;public class MortgageApp {
+package cpsc2150.banking;
+
+import cpsc2150.banking.controllers.*;
+import cpsc2150.banking.views.*;
+
+/**
+ * This class holds main to run the program.
+ * @author Connor Love
+ * @since 4/5/2023
+ */
+public class MortgageApp {
+    public static void main(String [] args) {
+        IMortgageView view = new MortgageView();
+        IMortgageController controller = new MortgageController(view);
+        view.setController(controller);
+        controller.submitApplication();
+    }
 }
